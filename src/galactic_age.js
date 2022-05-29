358a1f6c7eceacd06a0d88620f520cb13f9871fa
liliia87 <lilia.krivelyova@gmail.com>
2022-05-29 13:59:56 -0700

    moved all prototypes to main class

--- src/galactic_age.js
+++ src/galactic_age.js
@@ -28,109 +28,108 @@ export default class Person {
     this.age = age;
     return Math.round(this.age/11.86);
   }
-}
 
-Person.prototype.lifestyleList = function(life_S){
+  lifestyleList = function(life_S){
 
-  let lifeS;
-  switch (life_S){
-    case "professional sport": 
-      lifeS = 5;
-      console.log("You choose - professional sport");
-      break;
-    case "sport":
-      lifeS = 10;
-      console.log("You choose - sport");
-      break;
-    case "no sport":
-      lifeS = 15;
-      console.log("You choose - no sport");
-      break;
-    default:
-      //console.log(`Sorry, we are out of ${lifeS}`);
-      lifeS = 0;
-      break;
+    let lifeS;
+    switch (life_S){
+      case "professional sport": 
+        lifeS = 5;
+        console.log("You choose - professional sport");
+        break;
+      case "sport":
+        lifeS = 10;
+        console.log("You choose - sport");
+        break;
+      case "no sport":
+        lifeS = 15;
+        console.log("You choose - no sport");
+        break;
+      default:
+        //console.log(`Sorry, we are out of ${lifeS}`);
+        lifeS = 0;
+        break;
+    }
+    return lifeS;
   }
-  return lifeS;
-}
 
-Person.prototype.activityLevel = function(activity_L){
+  activityLevel = function(activity_L){
 
-  let lifeSactivityL;
-  switch (activity_L){
-    case "every day": 
-    lifeSactivityL = 5;
-      console.log("You choose - professional sport");
-      break;
-    case "one per week":
-      lifeSactivityL = 10;
-      console.log("You choose - sport");
-      break;
-    case "not often":
-      lifeSactivityL = 15;
-      console.log("You choose - no sport");
-      break;
-    default:
-      //console.log(`Sorry, we are out of ${lifeSactivityL}`);
-      lifeSactivityL = 0;
-      break;
+    let lifeSactivityL;
+    switch (activity_L){
+      case "every day": 
+      lifeSactivityL = 5;
+        console.log("You choose - professional sport");
+        break;
+      case "one per week":
+        lifeSactivityL = 10;
+        console.log("You choose - sport");
+        break;
+      case "not often":
+        lifeSactivityL = 15;
+        console.log("You choose - no sport");
+        break;
+      default:
+        //console.log(`Sorry, we are out of ${lifeSactivityL}`);
+        lifeSactivityL = 0;
+        break;
+    }
+    return lifeSactivityL;
   }
-  return lifeSactivityL;
-}
 
-Person.prototype.calculateAverage = function (life_S, activity_L){
-  let lifeStyle = this.lifestyleList(life_S);
-  let activityL = this.activityLevel(activity_L);
-  let result = lifeStyle + activityL;
-  let aver;
-  if (result == 10 || result == 20){
-   aver = 50;
+  calculateAverage = function (life_S, activity_L){
+    let lifeStyle = this.lifestyleList(life_S);
+    let activityL = this.activityLevel(activity_L);
+    let result = lifeStyle + activityL;
+    let aver;
+    if (result == 10 || result == 20){
+     aver = 50;
+    }
+    else if (result == 15){
+     aver = 80;
+    }
+    else if (result == 25){
+      aver = 75;
+    }
+    else
+      aver = 60;
+    return aver;
   }
-  else if (result == 15){
-   aver = 80;
-  }
-  else if (result == 25){
-    aver = 75;
-  }
-  else
-    aver = 60;
-  return aver;
-}
 
-Person.prototype.calculateMercuryAver = function(age, lifeS, activityL){
-  let Mercury_age = this.calculateMercury(age);
-  let life_S = lifeS;
-  let activity_L = activityL;
-  let aver_Age = this.calculateAverage(life_S, activity_L);
-  let difference;
-  if (Mercury_age > aver_Age){
-    difference = Mercury_age - aver_Age;
-    return `You will live on Mercury on ${Math.round(difference)} year more`;
+  calculateMercuryAver = function(age, lifeS, activityL){
+    let Mercury_age = this.calculateMercury(age);
+    let life_S = lifeS;
+    let activity_L = activityL;
+    let aver_Age = this.calculateAverage(life_S, activity_L);
+    let difference;
+    if (Mercury_age > aver_Age){
+      difference = Mercury_age - aver_Age;
+      return `You will live on Mercury on ${Math.round(difference)} year more`;
+    }
+    else {
+      difference = aver_Age - Mercury_age;
+      return `You will live on Mercury on ${Math.round(difference)} year less`;
+    }
+    
   }
-  else {
-    difference = aver_Age - Mercury_age;
-    return `You will live on Mercury on ${Math.round(difference)} year less`;
-  }
-  
-}
 
-Person.prototype.calculateVenusAver = function(age){
-  let Venus_age = this.calculateVenus(age);
-  let life_S = "no sport";
-  let activity_L = "not often";
-  let aver_Age = this.calculateAverage(life_S, activity_L);
-  let difference;
-  if (Venus_age > aver_Age){
-    difference = Venus_age - aver_Age;
-    return `You will live on Venus on ${Math.round(difference)} year more`;
+  calculateVenusAver = function(age){
+    let Venus_age = this.calculateVenus(age);
+    let life_S = "no sport";
+    let activity_L = "not often";
+    let aver_Age = this.calculateAverage(life_S, activity_L);
+    let difference;
+    if (Venus_age > aver_Age){
+      difference = Venus_age - aver_Age;
+      return `You will live on Venus on ${Math.round(difference)} year more`;
+    }
+    else {
+      difference = aver_Age - Venus_age;
+      return `You will live on Venus on ${Math.round(difference)} year less`;
+    }
   }
-  else {
-    difference = aver_Age - Venus_age;
-    return `You will live on Venus on ${Math.round(difference)} year less`;
-  }
-}
 
-  Person.prototype.calculateMarsAver = function(age){
+  calculateMarsAver = function(age){
     let Mars_age = this.calculateMars(age);
     let life_S = "professional sport";
     let activity_L = "one per week";
@@ -147,7 +146,7 @@ Person.prototype.calculateVenusAver = function(age){
 
 }
 
-Person.prototype.calculateJupiterAver = function(age, lifeS, activityL){
+calculateJupiterAver = function(age, lifeS, activityL){
   let Jupiter_age = this.calculateJupiter(age);
   let life_S = lifeS;
   let activity_L = activityL;
@@ -157,3 +156,10 @@ Person.prototype.calculateJupiterAver = function(age, lifeS, activityL){
     return `You will live on Jupiter on ${Math.round(difference)} year less`;
 
 }
+  
+}
+
+
+
+
+
